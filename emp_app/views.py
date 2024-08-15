91150040449b3e3806a67d65aaf0d7e37aef403a
Bisaka Shrestha <shrestha.bisaka18@gmail.com>
2024-08-15 21:39:35 +0545

    Updated the code with new features

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from .models import Employee, Role, Department
 from datetime import datetime
+from django.db.models import Q 
 # Create your views here.
 def index(request):
     return render(request, 'index.html' )
@@ -33,23 +34,41 @@ def add_emp(request):
         return HttpResponse("Error Occured!")
 
 
-def remove_emp(request, emp_id=0):
+def remove_emp(request, emp_id = 0):
     if emp_id:
         try:
             emp_to_be_removed = Employee.objects.get(id=emp_id)
             emp_to_be_removed.delete()
             return HttpResponse("Employee removed successfully")
-        except Employee.DoesNotExist:
+        except :
             return HttpResponse("Employee does not exist.")
-    
+        
     emps = Employee.objects.all()
     context = {
         'emps': emps
     }
-    print(context)
     return render(request, 'view_remove_emp.html', context)
 
 
 
 def filter_emp(request):
-    return render(request, 'view_filter_emp.html' )
+    if request.method == 'POST':
+        name = request.POST['name'] 
+        dept = request.POST['dept']
+        role = request.POST['role'] 
+        emps = Employee.objects.all()
+        if name:
+            emps = emps.filter(Q(first_name__icontains = name)| Q(last_name__icontains = name))
+        if dept:
+            emps = emps.filter(dept__name__icontains=dept)
+        if role:
+            emps = emps.filter(role__name__icontains=role)
+        
+        context = {
+        'emps': emps
+        }
+        return render(request, 'view_all_emp.html', context)
+    elif request.method == 'GET':
+        return render(request, 'view_filter_emp.html' )
+    else:
+        return HttpResponse("Error")
